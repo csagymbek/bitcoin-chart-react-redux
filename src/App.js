8f8db217e6e720e247c6ebe5f3384ea07b9aae5a
csagymbek <csagymbek@gmail.com>
2021-01-23 21:43:17 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Line } from "react-chartjs-2";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getData } from "./actions/bitcoinActions";
 
 function App() {
+  const [num, setNum] = useState(15);
+
+  const dispatch = useDispatch();
+
+  const state = useSelector((state) => state.bitcoin);
+
+  const fetchData = (time) => {
+    // fetch data from redux using time
+    dispatch(
+      getData({
+        time,
+        number: num,
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="btns-wrapper">
+        <button onClick={() => fetchData("1min")}>1 Min</button>
+        <button onClick={() => fetchData("5min")}>5 Min</button>
+        <button onClick={() => fetchData("15min")}>15 Min</button>
+        <input type="text" onChange={(e) => setNum(e.target.value)} />
+        {state.loading && <p>Loading...</p>}
+      </div>
+      <div className="chart-wrapper">
+        <Line data={state.data} />
+      </div>
     </div>
   );
 }
